577b7b1646e10b8acc134a6fd5befba1fe959044
Jon Stokes <jon.stokes@asynchrony.com>
2017-08-17 16:15:06 -0600

    US Zip Codes bruh

--- src/br/cep/cep.js
+++ src/br/cep/cep.js
@@ -3,18 +3,18 @@
 var StringMask = require('string-mask');
 var maskFactory = require('mask-factory');
 
-var cepMask = new StringMask('00000-000');
+var cepMask = new StringMask('00000-0000');
 
 module.exports = maskFactory({
 	clearValue: function(rawValue) {
-		return rawValue.toString().replace(/[^0-9]/g, '').slice(0, 8);
+		return rawValue.toString().replace(/[^0-9]/g, '').slice(0, 9);
 	},
 	format: function(cleanValue) {
 		return (cepMask.apply(cleanValue) || '').replace(/[^0-9]$/, '');
 	},
 	validations: {
 		cep: function(value) {
-			return value.toString().trim().length === 8;
+			return value.toString().trim().length === 9;
 		}
 	}
 });
